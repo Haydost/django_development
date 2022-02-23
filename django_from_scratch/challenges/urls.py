89797ae685f0f6e3bce37e2b116049060eca88f9
Haydost <alimohammadi1991@outlook.com>
2022-02-23 13:18:57 +0330

    adding name argument to the path for using reverse function in views

--- django_from_scratch/challenges/urls.py
+++ django_from_scratch/challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("<int:month>", views.Monthly_num),
-    path("<str:month>", views.Monthly_str)
+    path("<str:month>", views.Monthly_str, name="month-challenge")
 ]
